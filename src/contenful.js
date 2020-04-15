4d6acb5f598275f8ec6325cb0337f5dfe449f0ec
Okafor Irene <okaforirenen@gmail.com>
2020-04-15 19:43:41 +0100

    added env config

--- src/contenful.js
+++ src/contenful.js
@@ -2,7 +2,7 @@ import { createClient } from "contentful";
 
 export default createClient(
     {
-        space:"mc9ud6pwt6az",
-        accessToken:"v70dNxKQUaUzh_q4DWssv7i7n5ibkqXH3HxU3Xm4C0I"
+        space:process.env.REACT_APP_SPACE,
+        accessToken:process.env.REACT_APP_KEY
     }
 );
